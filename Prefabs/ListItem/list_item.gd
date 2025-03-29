d2e16704f24f56ccf6702c18fc9fb6b9792ce59f
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2025-03-29 12:35:19 +0800

    fix editing item bug bug bug bug bubg gugb

--- Prefabs/ListItem/list_item.gd
+++ Prefabs/ListItem/list_item.gd
@@ -2,9 +2,12 @@ extends Control
 
 @onready var item_label: Node = $Label
 
+var label_without_index: String = ""
+
 var list_position: int = 0
 
 func initialize_string(_text: String, _index: int) -> void:
+	label_without_index = _text
 	item_label.text = str(_index) + ". " + _text
 	list_position = _index - 1
 	self.custom_minimum_size = Vector2(1080, 150)
@@ -13,5 +16,5 @@ func _on_delete_button_pressed() -> void:
 	AppManager.remove_index_from_list(list_position)
 
 func _on_edit_button_pressed() -> void:
-	var _new_label: String = await AppManager.create_string_input("Edit item:", item_label.text)
+	var _new_label: String = await AppManager.create_string_input("Edit item:", label_without_index)
 	AppManager.update_index_in_list(list_position, _new_label)
